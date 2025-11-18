bfea8202172257b50eb38f17480eecd5e0e0d4d1
GaelzonMS <324244315@alumno.enp.unam.mx>
2025-11-17 18:20:10 -0600

    Se acabo el apartado de comidas

--- RestauranteOax/src/restaurante/P4MartinezSantiagoGaelOax.java
+++ RestauranteOax/src/restaurante/P4MartinezSantiagoGaelOax.java
@@ -16,9 +16,14 @@ import javax.swing.*;
 public class P4MartinezSantiagoGaelOax extends JFrame implements ActionListener {
 
     private JPanel panelMenus, panelComida;
+
     private ImageIcon atole, tamalOax, tlayuyas;
     private JLabel etiqAtole, etiqTamal, etiqTlay, etiquetaDesayuno, nomAtole, nomTamal, nomTlay;
     private JButton butAtole, butTamal, butTlay;
+
+    private ImageIcon sopaPiedra, cecina, chapulines;
+    private JLabel etiqSopa, etiqCecina, etiquChapulin, etiqComidas, nomSopa, nomCecina, nomChapulin;
+    private JButton butSopa, butCecina, butChapulin;
 
     public static void main(String[] args) {
         P4MartinezSantiagoGaelOax marco = new P4MartinezSantiagoGaelOax();
@@ -55,11 +60,28 @@ public class P4MartinezSantiagoGaelOax extends JFrame implements ActionListener 
         etiquetaDesayuno.setBackground(colorCarnita);
         panelMenus.add(etiquetaDesayuno);
 
-        //instanciamos y botones imagenes de los desayunos
-        tamalOax = new ImageIcon(new ImageIcon("tamalesOax.jpg").getImage().getScaledInstance(160, 120, Image.SCALE_DEFAULT));
-        atole = new ImageIcon(new ImageIcon("atoles.jpg").getImage().getScaledInstance(160, 120, Image.SCALE_DEFAULT));
-        tlayuyas = new ImageIcon(new ImageIcon("tlayudas.jpg").getImage().getScaledInstance(160, 120, Image.SCALE_DEFAULT));
+        //instanciamos imagenes de los desayunos
+        tamalOax = new ImageIcon(new ImageIcon("tamalesOax.jpg").getImage().getScaledInstance(128, 96, Image.SCALE_DEFAULT));
+        atole = new ImageIcon(new ImageIcon("atoles.jpg").getImage().getScaledInstance(128, 96, Image.SCALE_DEFAULT));
+        tlayuyas = new ImageIcon(new ImageIcon("tlayudas.jpg").getImage().getScaledInstance(128, 96, Image.SCALE_DEFAULT));
 
+        etiqAtole = new JLabel(); // img atoles
+        etiqAtole.setIcon(atole);
+        etiqAtole.setBounds(30,80, 500, 500);
+        etiqAtole.setAlignmentX(LEFT_ALIGNMENT);
+        panelMenus.add(etiqAtole);
+
+        etiqTamal = new JLabel(); // img tamales oaxaqueños
+        etiqTamal.setIcon(tamalOax);
+        etiqTamal.setBounds(30,80, 500, 500);
+        panelMenus.add(etiqTamal);
+
+        etiqTlay = new JLabel(); //tlayudas
+        etiqTlay.setIcon(tlayuyas);
+        etiqTlay.setBounds(30,80, 500, 500);
+        panelMenus.add(etiqTlay);
+
+        // instanciamos botones para ordenar cada desayuno
         butAtole = new JButton("Ordenar");
         butAtole.setFont(fuenteSerief);
         butAtole.setBackground(colorVerdeOscuro);
@@ -73,43 +95,99 @@ public class P4MartinezSantiagoGaelOax extends JFrame implements ActionListener 
         butTlay.setBackground(colorVerdeOscuro);
         butTlay.addActionListener(this);
 
-        // botones y etiquetas de imagen de cada platillo
-        etiqAtole = new JLabel(); // atoles
-        etiqAtole.setIcon(atole);
-        etiqAtole.setBounds(30,80, 500, 500);
-        etiqAtole.setAlignmentX(LEFT_ALIGNMENT);
+        // botones y etiquetas de cada platillo
         nomAtole = new JLabel("Atoles \n (chocolate, arroz, guayaba)");
         nomAtole.setFont(fuenteSerief);
         nomAtole.setOpaque(true);
         nomAtole.setBackground(colorCarnita);
-        panelMenus.add(etiqAtole);
         panelMenus.add(nomAtole);
         panelMenus.add(butAtole);
 
-        etiqTamal = new JLabel(); //tamales oaxaqueños
-        etiqTamal.setIcon(tamalOax);
-        etiqTamal.setBounds(30,80, 500, 500);
         nomTamal = new JLabel("Tamales Oaxaqueños \n (verde, mole, dulce)");
         nomTamal.setFont(fuenteSerief);
         nomTamal.setOpaque(true);
         nomTamal.setBackground(colorCarnita);
-        panelMenus.add(etiqTamal);
         panelMenus.add(nomTamal);
         panelMenus.add(butTamal);
 
-        etiqTlay = new JLabel(); //tlayudas
-        etiqTlay.setIcon(tlayuyas);
-        etiqTlay.setBounds(30,80, 500, 500);
         nomTlay = new JLabel("Tlayudas");
         nomTlay.setFont(fuenteSerief);
         nomTlay.setOpaque(true);
         nomTlay.setBackground(colorCarnita);
-        panelMenus.add(etiqTlay);
         panelMenus.add(nomTlay);
         panelMenus.add(butTlay);
 
-        ventana.add(panelMenus);
+        //------------------Espacio de comidas--------------------
+        etiqComidas = new JLabel("Comidas");
+        etiqComidas.setFont(fuenteSerief30);
+        etiqComidas.setBounds(80, 40, 250, 20);
+        etiqComidas.setAlignmentX(CENTER_ALIGNMENT);
+        etiqComidas.setOpaque(true);
+        etiqComidas.setBackground(colorCarnita);
+        panelMenus.add(etiqComidas);
+
+        //instanciamos imagenes de las comidas
+        sopaPiedra = new ImageIcon(new ImageIcon("sopaPiedra.jpg").getImage().getScaledInstance(128, 96, Image.SCALE_DEFAULT));
+        chapulines = new ImageIcon(new ImageIcon("chapulines.jpg").getImage().getScaledInstance(128, 96, Image.SCALE_DEFAULT));
+        cecina = new ImageIcon(new ImageIcon("cecina.jpg").getImage().getScaledInstance(128, 96, Image.SCALE_DEFAULT));
+
+        etiqSopa = new JLabel(); // img atoles
+        etiqSopa.setIcon(sopaPiedra);
+        etiqSopa.setBounds(30,80, 500, 500);
+        etiqSopa.setAlignmentX(LEFT_ALIGNMENT);
+        panelMenus.add(etiqSopa);
+
+        etiquChapulin = new JLabel(); // img chapulines
+        etiquChapulin.setIcon(chapulines);
+        etiquChapulin.setBounds(30,80, 500, 500);
+        panelMenus.add(etiquChapulin);
+
+        etiqCecina = new JLabel(); // img cecina
+        etiqCecina.setIcon(cecina);
+        etiqCecina.setBounds(30,80, 500, 500);
+        panelMenus.add(etiqCecina);
+
+        // instanciamos botones para ordenar cada  comida
+        butSopa = new JButton("Ordenar");
+        butSopa.setFont(fuenteSerief);
+        butSopa.setBackground(colorVerdeOscuro);
+        butSopa.addActionListener(this);
+        butChapulin = new JButton("Ordenar");
+        butChapulin.setFont(fuenteSerief);
+        butChapulin.setBackground(colorVerdeOscuro);
+        butChapulin.addActionListener(this);
+        butCecina = new JButton("Ordenar");
+        butCecina.setFont(fuenteSerief);
+        butCecina.setBackground(colorVerdeOscuro);
+        butCecina.addActionListener(this);
+
+        // botones y etiquetas de cada platillo
+        nomSopa = new JLabel("Sopa de piedra \n (Plato grande 35$) \n (Plato chico $25)");
+        nomSopa.setFont(fuenteSerief);
+        nomSopa.setOpaque(true);
+        nomSopa.setBackground(colorCarnita);
+        panelMenus.add(nomSopa);
+        panelMenus.add(butSopa);
+
+        nomChapulin = new JLabel("Chapulines 40$");
+        nomChapulin.setFont(fuenteSerief);
+        nomChapulin.setOpaque(true);
+        nomChapulin.setBackground(colorCarnita);
+        panelMenus.add(nomChapulin);
+        panelMenus.add(butChapulin);
+
+        nomCecina = new JLabel("Cecina acompañada $50");
+        nomCecina.setFont(fuenteSerief);
+        nomCecina.setOpaque(true);
+        nomCecina.setBackground(colorCarnita);
+        panelMenus.add(nomCecina);
+        panelMenus.add(butCecina);
+
+        //-------------------Espacio de cenas-------------------------
+
         
+
+        ventana.add(panelMenus);
         panelComida = new JPanel();
         panelComida.setPreferredSize(new Dimension(500, 750));
         panelComida.setBackground(new Color(248, 63, 169));
